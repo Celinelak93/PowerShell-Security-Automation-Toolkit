54de32653b9821499ded503b4301f54006cd7ea9
Celinelak93 <tingtinglak@gmail.com>
2026-07-06 14:08:50 +0200

    Update Get-MFAStatus.ps1

--- EntraID/Get-MFAStatus.ps1
+++ EntraID/Get-MFAStatus.ps1
@@ -1,1 +1,15 @@
 
+Connect-MgGraph
+
+$users = Get-MgUser -All
+
+foreach ($user in $users) {
+
+    $methods = Get-MgUserAuthenticationMethod `
+        -UserId $user.Id
+
+    if ($methods.Count -eq 0) {
+        Write-Output $user.UserPrincipalName
+    }
+}
+
